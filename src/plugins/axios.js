2f9912b0c2c77eea90ef632bf19d54c63dc1bdb2
Bayu Sarifudin <bayusarifudin@gmail.com>
2023-11-21 13:03:15 +0700

    delete unused page, add plugins

---
+++ src/plugins/axios.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import { getCookies, delCookies } from './cookies'
+
+const baseUrl = import.meta.env.VITE_BASE_URL
+
+const baseApi = axios.create({
+	baseUrl
+})
+
+baseApi.interceptors.request.use(
+	(config) => {
+		const token = getCookies('CERT')
+		if (token) config.headers.Authorization = `Bearer ${token}`
+		else {
+			delCookies('CERT')
+			delete config.headers.Authorization
+		}
+		return config
+	},
+	(error) => {
+		throw error
+	}
+)
+
+baseApi.interceptors.response.use(
+	(response) => response.data,
+	(error) => {
+		switch (error.response.status) {
+			case 401:
+				delCookies('CERT')
+				break
+			default:
+				break
+		}
+		throw error?.response?.data ?? error
+	}
+)
+
+export { baseApi }
